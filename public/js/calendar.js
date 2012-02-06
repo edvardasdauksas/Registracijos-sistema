0497c1f7b0632e191ad224cf088770ecb205adce
unknown <edvardasas@gmail.com>
2012-02-06 23:38:50 +0200

    Calendar marks current time, cannot select reserved slots, calendar generates days by month

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -3,13 +3,19 @@ var employee_id = 1;
 var procedure_id = 1;
 var timeslot_id = 1;
 var user_id = 1;
+var date = new Date();
+var month = date.getMonth();
+var day = date.getDate();
+var hour = date.getHours();
+var minute = date.getMinutes();
 
 var monthNames = [ "Sausis", "Vasaris", "Kovas", "Balandis", "Gegužė", "Birželis",
     "Liepa", "Rupjūtis", "Rugsėjis", "Spalis", "Lapkritis", "Gruodis" ];
+var dayCount = [31,28,31,30,31,30,31,31,30,31,30,31];
 	
 	
 $(document).ready(function() {
-
+	$('.current-time').css('top', ((hour  - 8) * 40 + minute / 1.5));
 	$('.user-selection').css('height', 20 * slot_size);
 
 	for(var i = 1; i <= 14; i++) {
@@ -17,12 +23,21 @@ $(document).ready(function() {
 	}
 	
 	for(var i = 0; i < 12; i++) {
-		$('.months').append('<div class="month m-selected" id="month-' + i + '">' + monthNames[i] + '</div>');
+		$('.months').append('<div class="month" id="month-' + i + '">' + monthNames[i] + '</div>');
+		if(i == month) {
+			$('#month-' + i).addClass('m-selected');
+		}
+		$('#month-' + i).click(function() {
+			$('.month').removeClass('m-selected');
+			$(this).addClass('m-selected');
+			month = $(this).attr('id').substring(6);
+			appendDays();
+			getDaySlots();
+		});
 	}
 	
-	for(var i = 1; i <= 31; i++) {
-		$('.days').append('<div class="day" id="day-' + i + '">' + i + '</div>')
-	}
+
+	appendDays();
 
 	
 	$('#reg').click(function() {
@@ -39,38 +54,64 @@ $(document).ready(function() {
 	
 	for(var i = 1; i <= 28; i++) {
 		$('.grid').append('<div class="time-slot" id="time-slot-' + i + '"></div>');
+	}
+	
+	for(var i = 1; i <= 28; i++) {
+		if(checkSlot(i)) {
 			$('#time-slot-' + i).hover(function() {
-				$('.user-selection').addClass('visible');
 				if(!$('.dialog').hasClass('visible')) {
 					$('.user-selection').css('top',($(this).offset().top - 185));
-				}
-				
-				
-				var cool = true;
-					for(var j =0; j<3; j++) {
-						if($('#time-slot-' + (i +j)).hasClass('reserved')) {
-							cool = false;
-						}
-					}
-					if(!cool) {
-						$('.user-selection').addClass('error');
-					} else {
-						$('.user-selection').addClass('ok');
-						
-					}
-				}			
-			, function(){
-				if(!$('.dialog').hasClass('visible')) {
-					$('.user-selection').removeClass('visible');
-				}
-			}).click(function() {
+				}		
+			}			
+			, function(){}).click(function() {
+				$('.user-selection').css('top',($(this).offset().top - 185));
 				$('.dialog').addClass('visible');
 				$('.dialog').css('top',($(this).offset().top - 365));
 			});
+		}
 	}
 	
 	$('#cancel-reg').click(function() {
 		$('.dialog').removeClass('visible');
 	});
+	 
+	
+	
 	
 });
+
+function getDaySlots() {
+	//---- ajax shit that return all time slots
+}
+
+
+function checkSlot(id) {
+	var valid = true;
+	for(var i = 0; i < slot_size; i++) {
+		if($('#time-slot-' + (id + i)).hasClass('reserved')) {
+			valid = false;
+		}
+	}
+	
+	if(id + slot_size - 1 > 28) {
+		valid = false;
+	}
+	
+	return valid;
+}
+
+function appendDays() {
+	$('.days').html('');
+	for(var i = 1; i <= dayCount[month]; i++) {
+		$('.days').append('<div class="day" id="day-' + i + '">' + i + '</div>');
+		if(i == day) {
+			$('#day-' + i).addClass('d-selected');
+		}
+		$('#day-' + i).click(function() {
+			$('.day').removeClass('d-selected');
+			$(this).addClass('d-selected');
+			day = $(this).attr('id').substring(4);
+			getDaySlots();
+		});
+	}
+}
