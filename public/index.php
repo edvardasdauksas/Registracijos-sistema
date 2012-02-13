34c0363a454d10c164e6df39430729250ba30da4
Mantas <mojavosas@gmail.com>
2012-02-13 16:01:14 +0200

    navigacija, sulauziau bloga bloga kalendoriaus css, clear floats index.php

--- public/index.php
+++ public/index.php
@@ -3,28 +3,36 @@
     
     if(isset($_COOKIE['btyprlusr']) && isset($_COOKIE['btyprlpsw']) && !isset($_SESSION['id'])) {
         $_SESSION['id'] = $_COOKIE['btyprlusr'];
+      
     }
 ?>
-
+<!DOCTYPE HTML>
 <html>
   <head>
     <meta charset="utf-8">
     <link href="css/jquery-ui.css" rel="stylesheet" type="text/css"/>
+    <link type="text/css" rel="stylesheet" href="css/styles.css" />
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/jquery-ui.min.js"></script>
     <script type="text/javascript" src="js/scripts.js"></script>
-    <link type="text/css" rel="stylesheet" href="css/styles.css" />
   </head>
 	<body>
 		<div class="main">
-			<header></header>
+			<header><h1>WHAT WHAT WHAT WHAT WHA no</h1></header>
+      <nav>
+        <?php
+          $nav = include "../app/nav.php";
+          echo $nav;
+        ?>
+      </nav>
 			<div class="content">
 				<div id="stylist">
+        
 				</div>
-          <div id="procedure">
-          </div>
+        <div id="procedure">
+        
         </div>
-			</div>
-		</div>
+      </div>
+    </div>
 	</body>
 </html>
